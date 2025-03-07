f001903a1d77282b7a6167197fabb37d7f025be5
Abhinav TIwari <abhinavtiwari021@gmail.com>
2025-03-07 19:34:08 +0530

    "Reverse Of a Number"

---
+++ src/JavaBasics/ReverseOfNo.java
@@ -0,0 +1,22 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class ReverseOfNo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = sc.nextInt();
+        int s = 0;
+        System.out.print("Reverse of the number is: ");
+        while(n>0)
+        {
+
+            s=n%10;
+            n=n/10;
+            System.out.print(s);
+        }
+
+    }
+}
