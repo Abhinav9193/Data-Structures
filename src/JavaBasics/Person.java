dec46d12044c524817de91ab50ab72989cf1fa9a
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-03-19 18:25:15 +0530

    Creating an object

---
+++ src/JavaBasics/Person.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class Person {
+
+    int age = 21;
+
+    public static void main(String[] args) {
+        Person Abhinav = new Person();
+        System.out.println("The age of the person is: " + Abhinav.age);
+    }
+}
