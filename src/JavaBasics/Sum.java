bf3408f527c640515fccf9c7dd0274ea3deabed7
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-03-20 19:36:30 +0530

    Calling a method through param

---
+++ src/JavaBasics/Sum.java
@@ -0,0 +1,22 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Sum {
+    int add(int a, int b)
+    {
+        int s;
+        s = a+b;
+        return s;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        Sum sum = new Sum();
+        int ans = sum.add(x,y);
+        System.out.println("Sum of the numbers is: " + ans);
+    }
+}
