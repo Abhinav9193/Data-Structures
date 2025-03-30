1e66266e4a84e6cbf99a033b1d889312bb495a61
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-03-30 23:55:39 +0530

    Length of an Array

---
+++ src/JavaBasics/length.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class length {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,7,3,6,7,9,0,2};
+        int l = arr.length;
+        System.out.println("Length of the array is: " + " " + l);
+    }
+}
