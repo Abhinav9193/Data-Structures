4bbcca38f647bf51bb97da6cbfefdf8e5607fdc0
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-03-30 23:59:45 +0530

    2 D Array

---
+++ src/JavaBasics/TwoDArray.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][2];
+        arr = new int[][]{{3, 4}, {}, {}, {3, 4, 4}};
+        System.out.println(arr[0][1]);
+    }
+}
