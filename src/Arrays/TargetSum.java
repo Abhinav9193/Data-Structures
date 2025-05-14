e34da17d3623c48b66aafb9639848e4eea065aed
Abhinav TIwari <abhinavtiwari021@gmail.com>
2025-05-14 23:57:45 +0530

    Target Sum

--- src/Arrays/TargetSum.java
+++ src/Arrays/TargetSum.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TargetSum {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
         int n = sc.nextInt();
