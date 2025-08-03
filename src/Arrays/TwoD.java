5b6d1ead7d541fd5c775a8bfc310ee558a73c005
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-08-03 12:25:38 +0530

    Two Dimensional using literals

---
+++ src/Arrays/TwoD.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class TwoD {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {{2,3},{4,5},{6,8}};
+        System.out.println(arr[0][0]);  //2
+        System.out.println(arr[0][1]);  //3
+        System.out.println(arr[1][0]);  //4
+    }
+}
