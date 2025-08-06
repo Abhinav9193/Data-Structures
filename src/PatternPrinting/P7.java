96be865a423070a2599a6b107f0ff3dfb92fdac5
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-08-07 03:05:23 +0530

    Searching

---
+++ src/PatternPrinting/P7.java
@@ -0,0 +1,28 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class P7 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("The array is: ");
+        for(int i = 0; i <n ; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        
+    }
+}
