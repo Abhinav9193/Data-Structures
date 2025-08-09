4df39ab3ce13154580514d626d38d06f244ebf1c
Abhinav TIwari <abhinavtiwari021@gmail.com>
2025-08-09 23:26:22 +0530

    Searching

--- src/PatternPrinting/P8.java
+++ src/PatternPrinting/P8.java
@@ -11,6 +11,7 @@ public class P8 {
         int[] arr = new int[n];
 
         System.out.println("Enter the elements of the array: ");
+
         for(int i = 0; i < n; i++)
         {
             arr[i] = sc.nextInt();
