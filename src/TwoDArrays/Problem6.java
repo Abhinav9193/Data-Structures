df6d3e983cc6ebf4c0a236b930710c39fd344380
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-08-20 01:53:15 +0530

    Elements in spiral order

---
+++ src/TwoDArrays/Problem6.java
@@ -0,0 +1,58 @@
+package TwoDArrays;
+
+import java.util.Scanner;
+
+public class Problem6{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int m = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Spiral Order: ");
+        printSpiral(matrix, m, n);
+    }
+
+    static void printSpiral(int[][] matrix, int m, int n) {
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+}
