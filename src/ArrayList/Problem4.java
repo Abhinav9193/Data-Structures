014f96cdd41d53a965882bb49a366d7f94fcc997
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-08-21 13:13:19 +0530

    Random Datatype elements in a list

---
+++ src/ArrayList/Problem4.java
@@ -0,0 +1,17 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Problem4 {
+    public static void main(String[] args) {
+
+        ArrayList l = new ArrayList();
+        l.add("Abh");
+        l.add(true);
+        l.add(56);
+        l.add(89.6);
+
+        System.out.println(l);
+    }
+}
