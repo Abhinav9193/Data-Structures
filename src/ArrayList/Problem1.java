14a68c739d825ba7715c7f5c63939c976ffbed08
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-08-21 13:05:33 +0530

    Adding elements in ArrayList

---
+++ src/ArrayList/Problem1.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        List<Integer> l = new ArrayList<>();
+
+        l.add(2);
+        l.add(4);
+        l.add(6);
+        l.add(7);
+        l.add(12);
+
+        System.out.println(l);
+    }
+}
