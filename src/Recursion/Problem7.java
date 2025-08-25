d728f8ec7223768b002da43f77df64064079fcff
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-08-25 19:38:48 +0530

    2 methods for p^q

--- src/Recursion/Problem7.java
+++ src/Recursion/Problem7.java
@@ -9,16 +9,31 @@ public class Problem7 {
         int p = sc.nextInt();
         System.out.println("Enter the value of q: ");
         int q = sc.nextInt();
-        System.out.println(power(p,q));
+        System.out.println("Answer by method 1 is: " + method1(p,q));
+        System.out.println("Answer by method 2 is: " + method2(p,q));
     }
 
-    static int power(int p,int q)
+    static int method1(int p,int q)
     {
         if(q == 0)
         {
             return 1;
         }
-        int ans = power(p,q-1);
+        int ans = method1(p,q-1);
         return p*ans;
+    }
+
+    static int method2(int p,int q)
+    {
+        if(q==0)
+        {
+            return 1;
+        }
+
+        if(q%2==0)
+        {
+            return method2(p,q/2)*method2(p,q/2);
+        }
+        return p*method2(p,q/2)*method2(p,q/2);
     }
 }
