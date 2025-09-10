5dce2fc0834bdce664e79222a0cce5cd21233f71
Abhigit1803 <abhinavtiwari021@gmail.com>
2025-09-10 19:32:41 +0530

    Optimized Bubble Sorting

---
+++ src/Sorting/Problem2.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class Problem2 {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 2, 4, 1, 8, 9};
+        bubbleSort(arr);
+
+        System.out.print("The array after bubble sort is: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean flag = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                return;
+            }
+        }
+    }
+}
